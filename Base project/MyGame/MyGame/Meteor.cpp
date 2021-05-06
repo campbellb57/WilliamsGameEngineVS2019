8875a47d805df5001f4f26e317814a55fb6cb342
Bradley Campbell <campbellb@rsd407.org>
2021-05-05 17:00:27 -0700

    Game Engine Tutorial

---
+++ Base project/MyGame/MyGame/Meteor.cpp
@@ -0,0 +1,31 @@
+#include "Meteor.h"
+
+const float SPEED = 1.2f;
+
+Meteor::Meteor(sf::Vector2f pos)
+{
+	sprite_.setTexture(GAME.getTexture("Resources/meteor.png"));
+	sprite_.setPosition(pos);
+	sprite_.setScale(sf::Vector2f(0.05, 0.05 ));
+	assignTag("meteor");
+}
+
+void Meteor::draw()
+{
+	GAME.getRenderWindow().draw(sprite_);
+}
+
+void Meteor::update(sf::Time& elapsed)
+{
+	int msElapsed = elapsed.asMilliseconds();
+	sf::Vector2f pos = sprite_.getPosition();
+
+	if (pos.x < sprite_.getGlobalBounds().width * -1)
+	{
+		makeDead();
+	}
+	else
+	{
+		sprite_.setPosition(sf::Vector2f(pos.x - SPEED * msElapsed, pos.y));
+	}
+}
---
+++ Base project/MyGame/MyGame/Ship.cpp
@@ -0,0 +1,49 @@
+#include "ship.h"
+#include "Laser.h"
+
+const float SPEED = 0.3f;
+const int FIRE_DELAY = 200;
+
+void Ship::update(sf::Time& elapsed) {
+	sf::Vector2f pos = sprite_.getPosition();
+	float x = pos.x;
+	float y = pos.y;
+
+	int msElapsed = elapsed.asMilliseconds();
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))     y -= SPEED * msElapsed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))   y += SPEED * msElapsed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))   x -= SPEED * msElapsed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))  x += SPEED * msElapsed;
+		
+	sprite_.setPosition(sf::Vector2f(x, y));
+
+	if (fireTimer_ > 0)
+	{
+		fireTimer_ -= msElapsed;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && fireTimer_ <= 0)
+	{
+		fireTimer_ = FIRE_DELAY;
+
+		sf::FloatRect bounds = sprite_.getGlobalBounds();
+
+		float laserx = x + bounds.width;
+		float lasery = y + (bounds.height / 2.0f);
+
+		LaserPtr laser = std:: make_shared <Laser> (sf::Vector2f(laserx, lasery));
+		GAME.getCurrentScene().addGameObject(laser);
+	}
+}
+
+Ship::Ship()
+{
+	sprite_.setTexture(GAME.getTexture("Resources/ship.png"));
+	sprite_.setPosition(sf::Vector2f(100, 100));
+	sprite_.setScale(sf::Vector2f(0.2, 0.2));
+}
+
+void Ship::draw()
+{
+	GAME.getRenderWindow().draw(sprite_);
+}
